a8ce164e780afdf72a0f6b7cb42842f755368a99
Evaldas <evabur@icloud.com>
2021-02-09 13:20:36 +0200

    added admin

---
+++ src/Controller/Admin/ConferenceCrudController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\BooleanFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class ConferenceCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Conference::class;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+                ->add('year')
+                ->add('city')
+                ->add(BooleanFilter::new('isInternational'));
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
