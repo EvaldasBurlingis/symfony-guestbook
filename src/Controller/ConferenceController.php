3787c0b199c9c543e49c770042da35fb32519076
Evaldas <evabur@icloud.com>
2021-02-11 13:47:14 +0200

    added styled feedback forms

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,23 +3,28 @@
 namespace App\Controller;
 
 use Twig\Environment;
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
-use Doctrine\ORM\Query\Expr\Func;
-use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ConferenceController extends AbstractController
 {
 
     private $twig;
+    private $entityManager;
 
-    public function __construct(Environment $twig)
+    public function __construct(Environment $twig, EntityManagerInterface $entityManager)
     {
         $this->twig = $twig;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -35,16 +40,43 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/conference/{slug}", name="conference.show")
      */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir): Response
     {
+
+    $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+
+            // handle image upload
+            if ($photo = $form['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+                try {
+                    $photo->move($photoDir, $filename);
+                } catch (FileException $e) {
+                    // unable to upload
+                }
+                $comment->setPhotoFilename($filename);
+            }
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference.show', ['slug' => $conference->getSlug()]);
+        }
+
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
         return new Response($this->twig->render('conference/show.html.twig', [
-            'conference' => $conference,
-            'comments' => $paginator,
-            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
-            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'conference'   => $conference,
+            'comments'     => $paginator,
+            'previous'     => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next'         => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $form->createView()
         ]));
     }
 }
--- src/Form/CommentFormType.php
+++ src/Form/CommentFormType.php
@@ -3,22 +3,63 @@
 namespace App\Form;
 
 use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CommentFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('author')
-            ->add('text')
-            ->add('email')
-            ->add('createdAt')
-            ->add('photoFilename')
-            ->add('conference')
-        ;
+            ->add('author', null, [
+                'row_attr' => ['class' => 'mb-4'],
+                'label' => 'Name',
+                'label_attr' => ['class' => 'block text-xs text-indigo-500 mb-1'],
+                'attr' => [
+                    'class' => 'px-4 py-2',
+                    'placeholder' => 'name...'
+                    ]
+            ])
+            ->add('email', EmailType::class, [
+                'row_attr' => ['class' => 'mb-4'],
+                'label' => 'Email',
+                'label_attr' => ['class' => 'block text-xs text-indigo-500 mb-1'],
+                'attr' => [
+                    'class' => 'px-4 py-2',
+                    'placeholder' => 'email address...'
+                    ]
+            ])
+            ->add('text', TextareaType::class, [
+                'row_attr' => ['class' => 'mb-4'],
+                'label' => 'Feedback',
+                'label_attr' => ['class' => 'block text-xs text-indigo-500 mb-1'],
+                'attr' => [
+                    'class' => 'px-4 py-2',
+                    ]
+            ])
+            ->add('photo', FileType::class, [
+                'required'    => false,
+                'mapped'      => false,
+                'constraints' => [
+                    new Image(['maxSize' => '1024k'])
+                ],
+                'row_attr' => ['class' => 'mb-4'],
+                'label' => 'Conference photo',
+                'label_attr' => ['class' => 'block text-xs text-indigo-500 mb-1'],
+                'attr' => [
+                    'class' => 'px-4 py-2 bg-white',
+                    ]
+            ])
+            ->add('Save', SubmitType::class, [
+                'attr' => ['class' => 'px-4 py-2 bg-indigo-500 text-white']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
